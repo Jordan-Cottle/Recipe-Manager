217571653d6f8d38b305c28c0a3b2546258408b9
Jordan Cottle <jordancottle622@gmail.com>
2021-05-15 22:20:33 -0400

    Add coverage to tests

--- test.sh
+++ test.sh
@@ -2,5 +2,11 @@ pushd recipe-manager
 export PYTHONPATH="${PYTHONPATH};$(pwd)"
 popd
 
-pytest tests
+pytest --cov
+passed=$?
 
+if [[ $passed != 0 ]]; then
+    echo "Tests failed to meet acceptance criteria!"
+fi
+
+exit $passed

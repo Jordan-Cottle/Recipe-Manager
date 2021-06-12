9c8e07812e80197f344664799573df0cf0247933
Jordan Cottle <jordancottle622@gmail.com>
2021-06-11 23:42:27 -0400

    Add test for all model creation

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ import pytest
 
 from app import create_app
 
+from config import get_config
+
 
 @pytest.fixture(name="client")
 def create_test_client():
@@ -12,3 +14,10 @@ def create_test_client():
     app = create_app()
     with app.test_client() as test_client:
         yield test_client
+
+
+@pytest.fixture(name="config")
+def get_configuration():
+    """Fixture for easily providing config object to tests"""
+
+    return get_config()
--- tests/database/test_engine.py
+++ tests/database/test_engine.py
@@ -1,12 +1,19 @@
-""" Verify that the database can be connected to. """
+""" Verify that the database can be connected to and basic operations work. """
 
+import pytest
 from database import Database
+from database.models import setup_database, Recipe
 
 
-def test_database():
+@pytest.fixture(name="database")
+def create_database(config):
+    """Create an in memory database for tests."""
+
+    return Database(config.DATABASE_CONFIG)
+
+
+def test_database(database):
     """Test that database setup works independently from the flask app context."""
-
-    database = Database({"url": "sqlite:///:memory:"})
 
     with database.session as session:
         result = session.execute("select 'This is a test'").one()
@@ -15,3 +22,34 @@ def test_database():
         assert (
             result[0] == "This is a test"
         ), "The database should return the selected string"
+
+
+def test_model_create(database):
+    """Test models can be created and qu."""
+
+    setup_database(database.engine)
+
+    name = "Test Recipe"
+    description = "Test recipe for testing"
+    author = "Test User"
+
+    recipe = Recipe(
+        name=name,
+        description=description,
+        author=author,
+    )
+
+    with database.session as session:
+        session.add(recipe)
+
+    with database.session as session:
+        recipes = session.query(Recipe).all()
+
+        assert len(recipes) == 1, "Should have one recipe in database now"
+
+        recipe = recipes[0]
+
+        assert recipe.id == 1, "Recipes should have id auto assigned"
+        assert recipe.name == name
+        assert recipe.description == description
+        assert recipe.author == author
